9e61beef56ab1514155848fa974a7c9556e81095
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-08 05:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
+<<<<<<< HEAD
   return Object.assign({}, object, {[key]:value});
 }
 
@@ -18,4 +19,7 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
+=======
+  return Object.assign({}, object)
+>>>>>>> 4f2a83734dcc1a1bdb9210b4b91ac517262bfeb1
 }
